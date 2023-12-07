9dc9f6ee77cd4b541aa0a251a3208588c762046b
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-12-07 22:32:29 +0100

    nouveautés

--- BreakTheRules/src/components/Acceuil.jsx
+++ BreakTheRules/src/components/Acceuil.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "../styles/acceuil.css";
+import Navbar from "./Navbar";
 
 const Acceuil = () => {
   return (
     <div id="Acueil">
+      <Navbar/>
       <h1>
         We want to <br />
         change the rules
