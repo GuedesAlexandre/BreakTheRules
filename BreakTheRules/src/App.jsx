b7329d19b57ee3546962ce53af1c9859517df163
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-12-07 22:39:56 +0100

    new

---
+++ BreakTheRules/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Acceuil from "./components/Acceuil";
+import "./styles/acceuil.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Acceuil />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ BreakTheRules/src/components/Acceuil.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/acceuil.css";
+import Navbar from "./Navbar";
+
+const Acceuil = () => {
+  return (
+    <div id="Acueil">
+      <Navbar/>
+      <h1>
+        We want to <br />
+        change the rules
+      </h1>
+    </div>
+  );
+};
+
+export default Acceuil;
