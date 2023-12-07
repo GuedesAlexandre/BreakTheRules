1b8ad7364288cb3912b6df069f0fd5ab3dc376e6
Glenn Guillard <glenn.guillard.pros@gmail.com>
2023-12-07 22:40:43 +0100

    acc

--- BreakTheRules/src/components/Acceuil.jsx
+++ BreakTheRules/src/components/Acceuil.jsx
@@ -4,11 +4,14 @@ import Navbar from "./Navbar";
 
 const Acceuil = () => {
   return (
-    <div id="Acueil">
-      <h1>
-        We want to <br />
-        change the rules
-      </h1>
+    <div id="acueil">
+      <div id="sombre">
+        <Navbar />
+        <h1>
+          We want to <br />
+          change the rules
+        </h1>
+      </div>
     </div>
   );
 };
