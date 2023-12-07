9f14f7d8b8748f2e9cc6b3cd355b4d19fe9e3d50
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-12-07 21:54:44 +0100

    ajout des routes

--- BreakTheRules/src/App.jsx
+++ BreakTheRules/src/App.jsx
@@ -1,11 +1,28 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Acceuil from './components/Acceuil';
 
 function App() {
  
 
   return (
     <>
-     <h1>Coucou c'est nous</h1>
+    <Router>
+      <div>
+        {/* Header, Navbar, ou tout autre élément qui est présent sur toutes les pages */}
+        
+        <Routes>
+          {/* Route pour la page d'accueil */}
+          <Route path="/" element={<Acceuil/>} />
+
+          {/* Route pour l'autre page */}
+       
+
+          {/* Ajoutez d'autres routes si nécessaire */}
+        </Routes>
+        {/* Footer, ou tout autre élément qui est présent sur toutes les pages */}
+      </div>
+    </Router>
     </>
   )
 }
