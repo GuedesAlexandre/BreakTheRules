8f93f87b504dd5c9be875688cea6f3edf8e154f4
Glenn Guillard <glenn.guillard.pros@gmail.com>
2023-12-08 05:08:49 +0100

    Push

--- BreakTheRules/src/components/Navbar.jsx
+++ BreakTheRules/src/components/Navbar.jsx
@@ -1,33 +1,35 @@
-
-
-import React from 'react';
+import React from "react";
 import "../styles/acceuil.css";
-import logo from '../assets/Logo.svg';
-import Sea from '../assets/Sea.svg'
+import logo from "../assets/Logo.svg";
+import Sea from "../assets/Sea.svg";
+import burger from "../assets/burger-bar.png";
 
 const Navbar = () => {
-    return (
-        <>
-        <header>
-            <nav className="navbar">
-                <div className='logo'><img src={logo} alt="Logo" /></div>
-                <ul class="links">
-                <li>Home</li>
-                <li>Game</li>
-                <li>Projects</li>
-        
-                <li>About us</li>
-
-                
-                </ul>
-                <div className='addons'>
-                <button className="btn">Contact</button>
-                <img src={Sea}></img>
-                </div>
-            </nav>
-        </header>
-        </>
-    );
-}
+  const Open = () => {
+    document.querySelector(".menuMobile").style.transform = "translateX(0)";
+  };
+  return (
+    <>
+      <header>
+        <nav className="navbar">
+          <div className="logo">
+            <img src={logo} alt="Logo" />
+          </div>
+          <ul className="links">
+            <li>Home</li>
+            <li>Game</li>
+            <li>Projects</li>
+            <li>About us</li>
+          </ul>
+          <div className="addons">
+            <button className="btn">Contact</button>
+            <img src={Sea}></img>
+          </div>
+          <img src={burger} id="burger" onClick={Open}></img>
+        </nav>
+      </header>
+    </>
+  );
+};
 
 export default Navbar;
